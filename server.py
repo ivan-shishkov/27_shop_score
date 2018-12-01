c800d651c16b449676b432d9aa4ef5f4b0782d46
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-12-01 21:16:38 +0300

    Added getting info about orders processing in JSON format

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
+from shop_score import get_orders_processing_info
 
 app = Flask(__name__)
+
+
+@app.route('/orders-processing-info')
+def orders_processing_info():
+    return jsonify(orders_processing_info=get_orders_processing_info())
+
 
 @app.route('/')
 def score():
     return render_template('score.html')
+
 
 if __name__ == "__main__":
     app.run()
